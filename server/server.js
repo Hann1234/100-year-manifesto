72761fcd134131f7704fde2b2933ef4d78eb8a4c
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-27 14:05:12 -0500

    linked router files to their corisponding app on server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const missionStatementRouter = require('.routes/missionStatement.router');
+const mantrasRouter = require('.routes/mantras.router');
+const coreValuesRouter = require('.routes/coreValues.router');
+const forGoodRouter = require('.routes/forGood.router');
+const lifeGoalsRouter = require('.routes/lifeGoals.router');
+const guidingPrinciplesRouter = require('.routes/guidingPrinciples.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,12 +29,12 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/missionStatement', );
-app.use('/api/mantras', );
-app.use('/api/coreValues', );
-app.use('/api/forGood', );
-app.use('/api/lifeGoals', );
-app.use('/api/guidingPrinciples', );
+app.use('/api/missionStatement', missionStatementRouter );
+app.use('/api/mantras', mantrasRouter );
+app.use('/api/coreValues', coreValuesRouter );
+app.use('/api/forGood', forGoodRouter );
+app.use('/api/lifeGoals', lifeGoalsRouter );
+app.use('/api/guidingPrinciples', guidingPrinciplesRouter);
 
 
 // Serve static files
