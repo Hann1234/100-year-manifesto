98fcdbfb69a47aa48b29cf90043d1fbb7b729ad5
Benji Copan <benji1220@gmail.com>
2021-08-27 17:19:11 -0500

    filled out routes in trackVideo.router.js

--- server/routes/trackVideo.router.js
+++ server/routes/trackVideo.router.js
@@ -3,31 +3,93 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 /**
- * GET route template
+ * GET route for track_video table
  */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/', rejectUnauthenticated, (req, res) => {
+  console.log("in GET track_video table");
+  
+  const getQueryText = `
+    SELECT *
+    FROM "track_video
+    WHERE "user_id" = $1;
+  `;
+
+  pool
+    .query(getQueryText, [req.user.id])
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log('GET track_video table failed: ', err);
+      res.sendStatus(500);
+    });
 });
 
 /**
- * POST route template
+ * POST route for track_video table
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/',rejectUnauthenticated,   (req, res) => {
+  console.log("in POST track_video table");
+  
+  const getQueryText = `
+    INSERT INTO "track_video" ("user_id", "title", "last_activity", "complete")
+    VALUES ($1, $2, NOW(), false);
+  `;
+
+  pool
+    .query(getQueryText, [req.user.id, req.body.title])
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log('POST track_video table failed: ', err);
+      res.sendStatus(500);
+    });
 });
 
 /**
- * DELETE route template
+ * DELETE route for track_video table
  */
- router.delete('/', (req, res) => {
-  // DELETE route code here
+ router.delete('/:id',rejectUnauthenticated,  (req, res) => {
+  console.log("in DELETE track_video table");
+  
+  const getQueryText = `
+    DELETE FROM "track_video
+    WHERE "id" = $1;
+  `;
+
+  pool
+    .query(getQueryText, [req.params.id])
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log('DELETE track_video table failed: ', err);
+      res.sendStatus(500);
+    });
 });
 
 /**
- * PUT route template
+ * PUT route for track_video table
  */
- router.put('/', (req, res) => {
-  // PUT route code here
+ router.put('/:id',rejectUnauthenticated,  (req, res) => {
+  console.log("in PUT track_video table");
+  
+  const getQueryText = `
+    UPDATE "track_video
+    SET "last_activity" = NOW(), "complete" = $1
+    WHERE "id" = $2;
+  `;
+
+  pool
+    .query(getQueryText, [req.body.complete, req.params.id])
+    .then((response) => {
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log('PUT track_video table failed: ', err);
+      res.sendStatus(500);
+    });
 });
 
 
