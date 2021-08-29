ea3efbbf1a5041af0de0121625b661b1fa42b367
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-29 12:39:07 -0500

    created get route for lifeGoals

--- server/routes/lifeGoals.router.js
+++ server/routes/lifeGoals.router.js
@@ -7,9 +7,21 @@ const {rejectUnauthenticated,
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
-});
+ router.get('/',rejectUnauthenticated, (req, res) => {
+    const uId = req.user.id;
+    const qText = `
+      SELECT * FROM "life_goals" 
+      WHERE "user_id" = $1;
+      `;
+  
+    pool.query( qText, [uId])
+    .then((response) => { res.send(response.rows);
+    })
+    .catch((error) => {
+      console.log("Error GETting lifeGoals", error);
+      res.sendStatus(500);
+    });
+  });
 
 /**
  * POST route template
