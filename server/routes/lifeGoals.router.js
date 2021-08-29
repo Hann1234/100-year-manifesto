bbea1acdddceab26167d431b3ec8ba1885cb7bc8
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-29 12:32:12 -0500

    created put route for lifeGoals

--- server/routes/lifeGoals.router.js
+++ server/routes/lifeGoals.router.js
@@ -26,9 +26,23 @@ router.post('/', (req, res) => {
 /**
  * PUT route template
  */
- router.put('/', (req, res) => {
-  // PUT route code here
-});
+ router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const uId = req.user.id
+    const manifestoText = req.body
+    const qText = `
+      UPDATE "life_goals" 
+      SET "manifesto_text" = $1
+      WHERE "id" = $2 AND "user_id" = $3; 
+      `;
+    pool.query(qText, [manifestoText, id, uId])
+    .then(() => { res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log('Error PUTing lifeGoals', error);
+      res.sendStatus(500);
+    });  
+  });
 
 
 
