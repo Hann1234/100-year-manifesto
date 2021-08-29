afa718d5b26bebe7a9b43e7f8b007d399893d13c
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-29 12:17:37 -0500

    created get route for forGood

--- server/routes/forGood.router.js
+++ server/routes/forGood.router.js
@@ -5,9 +5,21 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
-});
+ router.get('/', (req, res) => {
+    const uId = req.user.id;
+    const qText = `
+      SELECT * FROM "for_good" 
+      WHERE "user_id" = $1;
+      `;
+  
+    pool.query( qText, [uId])
+    .then((response) => { res.send(response.rows)
+    })
+    .catch((error) => {
+      console.log("Error GETting forGood", error);
+      res.sendStatus(500);
+    });
+  });
 
 /**
  * POST route template
