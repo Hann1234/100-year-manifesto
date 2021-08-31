91ad56dd350ab6604f5435d5c7369c54293effbb
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-30 22:25:51 -0500

    Created the Material UI template prototype for all form pages

--- src/components/Mantras/Mantras.jsx
+++ src/components/Mantras/Mantras.jsx
@@ -36,6 +36,14 @@ const useStyles = makeStyles((theme) => ({
         whiteSpace: "wrap",
         marginBottom: theme.spacing(1)
       },
+      paper4: {
+        height: '62vh',
+        padding: theme.spacing(1),
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+        whiteSpace: "wrap",
+        marginBottom: theme.spacing(1)
+      },
     divider: {
       margin: theme.spacing(2, 0)
     }
@@ -59,12 +67,19 @@ function Mantras(props) {
             </Grid>
             <Grid item xs={8}>
               <Paper className={classes.paper}><h1>Mantras</h1>
-              <Grid item xs={6}>
-              <Paper className={classes.paper2}>Imaging the video will go here</Paper>
-              <Paper className={classes.paper3}>Imaging the video will go here</Paper>
+              <Grid container spacing={1}>
+                  <Grid item xs={6}>
+                  <Paper className={classes.paper2}><h2>The video will go here</h2></Paper>
+                  
+                  </Grid>
+                  <Grid item xs={6}>
+                  <Paper className={classes.paper3}>Having a framework for decisions is critical to living a life on purpose. Guiding principles set forth through “Words to Live By.” When life brings uncertainty through events, circumstances, & difficult moments having a compass to guide your decisions is necessary.
+These are one, two, or three word sentences or phrases.
+<h2>text stuff here</h2></Paper>
+                  </Grid>
               </Grid>
-              <Grid item xs={5}>
-              
+              <Grid item xs={12}>
+              <Paper className={classes.paper4}><h2>More Text put your inputs here tada this is could be our general layout</h2></Paper>
               </Grid>
               </Paper>
               
