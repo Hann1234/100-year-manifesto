503ad26e8141f2e0d262115214eb1ed4f52eeab9
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-30 22:08:32 -0500

    playing with MUI to figure out generic layout of the page

--- src/components/Mantras/Mantras.jsx
+++ src/components/Mantras/Mantras.jsx
@@ -1,20 +1,82 @@
 import React, { useState } from 'react';
 import {useSelector} from 'react-redux';
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+      display: "grid",
+      gridTemplateColumns: "repeat(12, 1fr)",
+      gridGap: theme.spacing(3)
+    },
+    paper: {
+      height: '100vh',
+      padding: theme.spacing(1),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+      whiteSpace: "wrap",
+      marginBottom: theme.spacing(1)
+    },
+    paper2: {
+        height: '24vh',
+        padding: theme.spacing(1),
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+        whiteSpace: "wrap",
+        marginBottom: theme.spacing(1)
+      },
+      paper3: {
+        height: '24vh',
+        padding: theme.spacing(1),
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+        whiteSpace: "wrap",
+        marginBottom: theme.spacing(1)
+      },
+    divider: {
+      margin: theme.spacing(2, 0)
+    }
+  }));
+
+
 function Mantras(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
   const store = useSelector((store) => store);
   const [heading, setHeading] = useState('Functional Component');
 
+  const classes = useStyles();
+
   return (
-    <div>
-      <h2>{heading}</h2>
-    </div>
+    <section>
+       
+        <div>
+         
+          <Grid container spacing={3}>
+            <Grid xs={4}>
+              <Paper className={classes.paper}>this is where the manifesto goes I do not know if we thought about this but almost all of our pages are going to follw a vary spacific grid layout so it should be atop priority to get that layout figured out so we can all have it for our pages</Paper>
+            </Grid>
+            <Grid item xs={8}>
+              <Paper className={classes.paper}><h1>Mantras</h1>
+              <Grid item xs={6}>
+              <Paper className={classes.paper2}>Imaging the video will go here</Paper>
+              <Paper className={classes.paper3}>Imaging the video will go here</Paper>
+              </Grid>
+              <Grid item xs={5}>
+              
+              </Grid>
+              </Paper>
+              
+            </Grid>
+          </Grid>
+        </div>
+    </section>
   );
 }
+  
+    
+
+
 
 export default Mantras;
