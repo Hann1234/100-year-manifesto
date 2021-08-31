95a1c09f87575bbd89b738e21a34f4085c42cdbd
Hann1234 <christopherjhannigan@gmail.com>
2021-08-31 12:00:59 -0500

    added chips, still need to complete

--- src/components/CoreValues/CoreValues.jsx
+++ src/components/CoreValues/CoreValues.jsx
@@ -69,6 +69,100 @@ function Intro() {
   const dispatch = useDispatch ();
   const classes = useStyles();
 
+  //Chip info
+  const [chipData, setChipData] = React.useState([
+    { key: 0, label: 'Acceptance' },
+    { key: 1, label: 'Accomplishment' },
+    { key: 2, label: 'Accountability' },
+    { key: 3, label: 'Accuracy' },
+    { key: 4, label: 'Achievement' },
+    { key: 5, label: 'Adaptability' },
+    { key: 6, label: 'Alertness' },
+    { key: 7, label: 'Altruism' },
+    { key: 8, label: 'Ambition' },
+    { key: 9, label: 'Amusement' },
+    { key: 10, label: 'Assertiveness' },
+    { key: 11, label: 'Attentive' },
+    { key: 12, label: 'Awareness' },
+    { key: 13, label: 'Balance' },
+    { key: 14, label: 'Beauty' },
+    { key: 15, label: 'Boldness' },
+    { key: 16, label: 'Bravery' },
+    { key: 17, label: 'Brilliance' },
+    { key: 18, label: 'Calm' },
+    { key: 19, label: 'Candor' },
+    { key: 20, label: 'Capable' },
+    { key: 21, label: 'Careful' },
+    { key: 22, label: 'Certainty' },
+    { key: 23, label: 'Challenge' },
+    { key: 24, label: 'Charity' },
+    { key: 25, label: 'Cleanliness' },
+    { key: 26, label: 'Clear' },
+    { key: 27, label: 'Clever' },
+    { key: 28, label: 'Comfort' },
+    { key: 29, label: 'Commitment' },
+    { key: 30, label: 'Common Sense' },
+    { key: 31, label: 'Communication' },
+    { key: 32, label: 'Community' },
+    { key: 33, label: 'Compassion' },
+    { key: 34, label: 'Competence' },
+    { key: 35, label: 'Concentration' },
+    { key: 36, label: 'Confidence' },
+    { key: 37, label: 'Connection' },
+    { key: 38, label: 'Consciousness' },
+    { key: 39, label: 'Consistency' },
+    { key: 40, label: 'Contentment' },
+    { key: 41, label: 'Contribution' },
+    { key: 42, label: 'Control' },
+    { key: 43, label: 'Conviction' },
+    { key: 44, label: 'Cooperation' },
+    { key: 45, label: 'Courage' },
+    { key: 46, label: 'Courtesy' },
+    { key: 47, label: 'Creation' },
+    { key: 48, label: 'Creativity' },
+    { key: 49, label: 'Credibility' },
+    { key: 50, label: 'Curiosity' },
+    { key: 51, label: 'jQuery' },
+    { key: 52, label: 'Polymer' },
+    { key: 53, label: 'React' },
+    { key: 54, label: 'Vue.js' },
+    { key: 55, label: 'Angular' },
+    { key: 56, label: 'jQuery' },
+    { key: 57, label: 'Polymer' },
+    { key: 58, label: 'React' },
+    { key: 59, label: 'Vue.js' },
+    { key: 60, label: 'Angular' },
+    { key: 61, label: 'jQuery' },
+    { key: 62, label: 'Polymer' },
+    { key: 63, label: 'React' },
+    { key: 64, label: 'Vue.js' },
+    { key: 65, label: 'Angular' },
+    { key: 66, label: 'jQuery' },
+    { key: 67, label: 'Polymer' },
+    { key: 68, label: 'React' },
+    { key: 69, label: 'Vue.js' },
+    { key: 70, label: 'Angular' },
+    { key: 71, label: 'jQuery' },
+    { key: 72, label: 'Polymer' },
+    { key: 73, label: 'React' },
+    { key: 74, label: 'Vue.js' },
+    { key: 75, label: 'Angular' },
+    { key: 76, label: 'jQuery' },
+    { key: 77, label: 'Polymer' },
+    { key: 78, label: 'React' },
+    { key: 79, label: 'Vue.js' },
+    { key: 80, label: 'Angular' },
+    { key: 81, label: 'jQuery' },
+    { key: 82, label: 'Polymer' },
+    { key: 83, label: 'React' },
+    { key: 84, label: 'Vue.js' },
+    { key: 85, label: 'Angular' },
+    { key: 86, label: 'jQuery' },
+    { key: 87, label: 'Polymer' },
+    { key: 88, label: 'React' },
+    { key: 89, label: 'Vue.js' },
+  ]);
+
   useEffect(() => {
     //need to retrieve video, image, and page details
 }, [])
@@ -135,6 +229,21 @@ const editName = (event) => {
                     For your 100 Year Manifesto, which are the ones that are in your heart of hearts. What are those core values?</h4>
                   {/* What is the "For Good" section renamed as? <button className="nextButton" onClick={() => history.push('/forgood')}>NEXT</button> */}
                 </center>
+                </Paper>
+                <Paper component="ul" className={classes.root}>
+                    {chipData.map((data) => {
+                        return (
+                            <li key={data.key}>
+                                <Chip
+                                label={data.label}
+                                className={classes.chip}
+                                clickable
+                                onClick={handleClick}
+                                color="primary"
+                                />
+                            </li>
+                        );
+                    })}
                 </Paper>
               </Grid>
             </Paper>
