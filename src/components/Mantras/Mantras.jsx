2b87ac214ebcd276a9e7d3100795b03e0a84c82f
Joshua Oren <joshuadavidoren@gmail.com>
2021-08-30 22:32:23 -0500

    formated code ajusted padding

--- src/components/Mantras/Mantras.jsx
+++ src/components/Mantras/Mantras.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
@@ -7,91 +7,101 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-      display: "grid",
-      gridTemplateColumns: "repeat(12, 1fr)",
-      gridGap: theme.spacing(3)
-    },
-    paper: {
-      height: '100vh',
-      padding: theme.spacing(1),
-      textAlign: "center",
-      color: theme.palette.text.secondary,
-      whiteSpace: "wrap",
-      marginBottom: theme.spacing(1)
-    },
-    paper2: {
-        height: '24vh',
-        padding: theme.spacing(1),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-        whiteSpace: "wrap",
-        marginBottom: theme.spacing(1)
-      },
-      paper3: {
-        height: '24vh',
-        padding: theme.spacing(1),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-        whiteSpace: "wrap",
-        marginBottom: theme.spacing(1)
-      },
-      paper4: {
-        height: '62vh',
-        padding: theme.spacing(1),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-        whiteSpace: "wrap",
-        marginBottom: theme.spacing(1)
-      },
-    divider: {
-      margin: theme.spacing(2, 0)
-    }
-  }));
-
+  container: {
+    display: "grid",
+    gridTemplateColumns: "repeat(12, 1fr)",
+    gridGap: theme.spacing(3),
+  },
+  paper: {
+    height: "100vh",
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "wrap",
+    marginBottom: theme.spacing(1),
+  },
+  paper2: {
+    height: "24vh",
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "wrap",
+    marginBottom: theme.spacing(1),
+  },
+  paper3: {
+    height: "24vh",
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "wrap",
+    marginBottom: theme.spacing(1),
+  },
+  paper4: {
+    height: "62vh",
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    whiteSpace: "wrap",
+    marginBottom: theme.spacing(1),
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+}));
 
 function Mantras(props) {
   const store = useSelector((store) => store);
-  const [heading, setHeading] = useState('Functional Component');
+  const [heading, setHeading] = useState("Functional Component");
 
   const classes = useStyles();
 
   return (
     <section>
-       
-        <div>
-         
-          <Grid container spacing={3}>
-            <Grid xs={4}>
-              <Paper className={classes.paper}>this is where the manifesto goes I do not know if we thought about this but almost all of our pages are going to follw a vary spacific grid layout so it should be atop priority to get that layout figured out so we can all have it for our pages</Paper>
-            </Grid>
-            <Grid item xs={8}>
-              <Paper className={classes.paper}><h1>Mantras</h1>
+      <div>
+        <Grid container spacing={3}>
+          <Grid xs={4}>
+            <Paper className={classes.paper}>
+              this is where the manifesto goes I do not know if we thought about
+              this but almost all of our pages are going to follw a vary
+              spacific grid layout so it should be atop priority to get that
+              layout figured out so we can all have it for our pages
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <Paper className={classes.paper}>
+              <h1>Mantras</h1>
               <Grid container spacing={1}>
-                  <Grid item xs={6}>
-                  <Paper className={classes.paper2}><h2>The video will go here</h2></Paper>
-                  
-                  </Grid>
-                  <Grid item xs={6}>
-                  <Paper className={classes.paper3}>Having a framework for decisions is critical to living a life on purpose. Guiding principles set forth through “Words to Live By.” When life brings uncertainty through events, circumstances, & difficult moments having a compass to guide your decisions is necessary.
-These are one, two, or three word sentences or phrases.
-<h2>text stuff here</h2></Paper>
-                  </Grid>
+                <Grid item xs={6}>
+                  <Paper className={classes.paper2}>
+                    <h2>The video will go here</h2>
+                  </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                  <Paper className={classes.paper3}>
+                    Having a framework for decisions is critical to living a
+                    life on purpose. Guiding principles set forth through “Words
+                    to Live By.” When life brings uncertainty through events,
+                    circumstances, & difficult moments having a compass to guide
+                    your decisions is necessary. These are one, two, or three
+                    word sentences or phrases.
+                    <h2>text stuff here</h2>
+                  </Paper>
+                </Grid>
               </Grid>
               <Grid item xs={12}>
-              <Paper className={classes.paper4}><h2>More Text put your inputs here tada this is could be our general layout</h2></Paper>
+                <Paper className={classes.paper4}>
+                  <h2>
+                    More Text put your inputs here tada this is could be our
+                    general layout
+                  </h2>
+                </Paper>
               </Grid>
-              </Paper>
-              
-            </Grid>
+            </Paper>
           </Grid>
-        </div>
+        </Grid>
+      </div>
     </section>
   );
 }
-  
-    
-
-
 
 export default Mantras;
