eeca07a7c90b2dcb061c96317e32bdf29ba5c5d4
Hann1234 <christopherjhannigan@gmail.com>
2021-08-31 11:33:09 -0500

    added basic text and outline to core values page

--- src/components/CoreValues/CoreValues.jsx
+++ src/components/CoreValues/CoreValues.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import Chip from '@material-ui/core/Chip';
 
 import Grid from "@material-ui/core/Grid";
 
@@ -49,6 +50,17 @@ const useStyles = makeStyles((theme) => ({
   divider: {
     margin: theme.spacing(2, 0),
   },
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    listStyle: 'none',
+    padding: theme.spacing(0.5),
+    margin: 0,
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
 }));
 
 function Intro() {
@@ -84,60 +96,44 @@ const editName = (event) => {
           <Grid item xs={8}>
             <Paper className={classes.paper}>
             <center>
-              <h1>Intro: Your 100 Year Manifesto</h1>
-                <h2>Welcome to The 100 Year Manifesto.  We are so glad you’re here.</h2>
-                <br></br>
-                <h2>Your 100 Year Manifesto is your framework for living.</h2>
-                <br></br>
-                <h2>For living with intentionality.  For living the life worthy of the calling you received.</h2>
+              <h1>Core Values</h1>
+                <h2>You can’t argue with your core values. They just are.  What are yours?</h2>
             </center>
               <Grid container spacing={1}>
                 <Grid item xs={6}>
                   <Paper className={classes.paper2}> 
-                  {/* under the video we will include Micks information add another Grid component for Micks info?*/}
                     <h2>The video will go here</h2>
-                    <br></br>
-                    {/* make Mick's information a separate card with circled avatar pic */}
-                    <h3>Instructor: Mick White</h3>
-                    <br></br>
-                    <h4>Founder - 100 Year Manifesto</h4>
-                    <br></br>
-                    <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/3152123/settings_images/FtJgiPhtTE6KMfcyTFIC_120903065_10158492341299373_1571312014878542166_o.jpg" alt="Mick White" />
-                    <h4>"To live with intentionality. A framework for living. A commitment to be who you are capable of being. A commitment to live a better life. Your life on purpose. Your business. Your life. For good.”</h4>
                   </Paper>
                 </Grid>
                 <Grid item xs={6}>
                   <Paper className={classes.paper3}>
-                    <h3>Logistics for the course:</h3>
+                    <center>
+                    <h4>Most companies have identified a set of core values. Oftentimes they post them on their walls as a reminder for their employees, their customers, & everyone they encounter.</h4>
                     <br></br>
-                    <h4>Each section of this course has a short video & a worksheet.</h4>
+                    <h4>If it makes sense for a business, it definitely makes sense for an individual to have a personal set of core values. Uncompromising, non-negotiables.</h4>
                     <br></br>
-                    <h4>The video is the content for the section.  The overview and philosophy behind each topic.</h4>
-                    <br></br>
-                    <h4>The worksheets make the 100 Year Manifesto yours.  Spend some time filling out each worksheet.  Creating your own 100 Year Manifesto.  The deeper you go, the more meaningful the 100 Year Manifesto you create for your life.</h4>
-                    <form onSubmit={editName}>
+                    <h4>Our values reveal themselves to us.</h4>
+                    </center>
+                    {/* <form onSubmit={editName}> add input to create new chips
                         <input
-                            className="name"
+                            className=""
                             value={name}
                             onChange={(event) => setName(event.target.value)}
                             placeholder="Please enter preferred name"
                         />
                         <button className="nameButton" type="submit">SAVE</button>
-                    </form>
+                    </form> */}
                   </Paper>
                 </Grid>
               </Grid>
               <Grid item xs={12}>
                 <Paper className={classes.paper4}>
                 <center>
-                  <h4>Questions? Send us an email at team@100yearmanifesto.com</h4>
-                  <br></br>
-                  <h4>When you complete the course, you’ll receive your own frame worthy 100 Year Manifesto.</h4>
-                  <br></br>
-                  <h4>You can also post it on Facebook, Twitter, Instagram, & LinkedIn.  Use the hashtag #my100yearmanifesto</h4>
-                  <br></br>
-                  <h4>It’s time to start living your 100 Year Manifesto!</h4>
-                  <button className="nextButton" onClick={() => history.push('/missionStatement')}>NEXT</button>
+                  <h4>What five core values have revealed themselves to you in the course of your life?
+                    Pick 4-5-6 core values. Ones that have revealed themselves to you in your life.
+                    The challenge here is this: there may be many core values that resonate with you.
+                    For your 100 Year Manifesto, which are the ones that are in your heart of hearts. What are those core values?</h4>
+                  {/* What is the "For Good" section renamed as? <button className="nextButton" onClick={() => history.push('/forgood')}>NEXT</button> */}
                 </center>
                 </Paper>
               </Grid>
