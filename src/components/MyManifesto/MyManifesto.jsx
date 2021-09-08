28f5ab3138766cfc388071c73054764881563d5b
Duy Dao <jimmydao@gmail.com>
2021-09-08 08:19:58 -0500

    Created MyManifesto.jsx file and imported Manifesto.

---
+++ src/components/MyManifesto/MyManifesto.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Manifesto from '../Manifesto/Manifesto';
+
+function MyManifesto() {
+    return (
+        <div>
+            <Manifesto/>
+        </div>
+    )
+}
+
+export default MyManifesto;
