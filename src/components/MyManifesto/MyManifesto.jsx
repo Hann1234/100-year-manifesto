9fa9416d700efc3d43b8ea5f4073c4cedefbbc2f
Duy Dao <jimmydao@gmail.com>
2021-09-08 09:47:31 -0500

    Created a print and export feature to MyManifesto Page

--- src/components/MyManifesto/MyManifesto.jsx
+++ src/components/MyManifesto/MyManifesto.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Manifesto from '../Manifesto/Manifesto';
+import { useReactToPrint } from 'react-to-print';
 
 function MyManifesto() {
+    const manifestoRef = useRef();
+    
+    const handlePrint = useReactToPrint({
+        content: () => manifestoRef.current,
+    });
+
     return (
         <center>
-            <div>
-                <Manifesto/>
+            <div ref={manifestoRef}>
+                <Manifesto />
+                <button onClick={handlePrint}>Print</button>
             </div>
         </center>
     )
