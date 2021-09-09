74f624c5c6ac6e3c46c30ed07bb25bc41f03c4dc
Duy Dao <jimmydao@gmail.com>
2021-09-09 14:08:59 -0500

    Trying to get hamburger menu to populate even when logged out.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,6 +10,7 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
+import LogInButton from "../LoginButton/LoginButton";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,11 +37,11 @@ function Nav() {
 
   if (user.id != null) {
     loginLinkData.path = "/user";
-    // loginLinkData.text = "Home";
   }
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+    console.log(`Is handleClick for burger menu activating?`, event);
   };
 
   const handleClose = () => {
@@ -65,16 +66,32 @@ function Nav() {
           <Link className="navLink" to="/login">
             Login / Register
           </Link>
+          // <div>
+          //   <MenuIcon
+          //     aria-controls="simple-menu"
+          //     aria-haspopup="true"
+          //     onClick={handleClick}
+          //   />
+          //   <Menu
+          //     id="simple-menu"
+          //     anchorEl={anchorEl}
+          //     keepMounted
+          //     open={Boolean(anchorEl)}
+          //     onClose={handleClose}
+          //   >
+          //     <LogInButton/>
+          //   </Menu> 
+          // </div>
         )}
 
         {/* If a user is logged in, show these links */}
         {user.id && (
           <div>
             <MenuIcon
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          />
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClick}
+            />
             <Menu
               id="simple-menu"
               anchorEl={anchorEl}
@@ -83,40 +100,36 @@ function Nav() {
               onClose={handleClose}
             >
               <MenuItem
-                  className="navLink"
-                  onClick={() => {
-                    history.push("/home");
-                    handleClose();
-                  }}
-                >
-                  Home
+                className="navLink"
+                onClick={() => {
+                  history.push("/home");
+                  handleClose();
+                }}
+              >
+                Home
               </MenuItem>
               <MenuItem
-                  className="navLink"
-                  onClick={() => {
-                    history.push("/myManifesto");
-                    handleClose();
-                  }}
-                >
-                  My Manifesto
+                className="navLink"
+                onClick={() => {
+                  history.push("/myManifesto");
+                  handleClose();
+                }}
+              >
+                My Manifesto
               </MenuItem>
               <MenuItem
-                  className="navLink"
-                  onClick={() => {
-                    history.push("/about");
-                    handleClose();
-                  }}
-                >
-                  About
+                className="navLink"
+                onClick={() => {
+                  history.push("/about");
+                  handleClose();
+                }}
+              >
+                About
               </MenuItem>
               <LogOutButton className="navLink" />
             </Menu>
           </div>
         )}
-
-        {/* <Link className="navLink" to="/about">
-          About
-        </Link> */}
       </div>
     </div>
   );
