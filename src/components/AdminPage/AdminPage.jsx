1464b11b554066fe58b8ce9c805f8174515fd2fa
Benji Copan <benji1220@gmail.com>
2021-09-10 09:04:17 -0500

    autocomplete field on admin page

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 function AdminPage() {
   const dispatch = useDispatch ();
   const user = useSelector((store) => store.user);
-  const userList = useSelector((store) => store.userList);
+  const userList = useSelector((store) => store.userList.userList);
+  const [selectedUser, setSelectedUser] = useState(user);
   
   // component is not listed in the progress bar
   dispatch({ type: "SET_NEXT_BUTTON", payload: -1 });
@@ -13,6 +16,8 @@ function AdminPage() {
       dispatch({ type: 'ADMIN_FETCH_USERS' });
   }, []);
 
+  console.log("user", user);
+  console.log("selectedUser", selectedUser);
   console.log("userList", userList);
   return (
     <section className="container">
@@ -21,7 +26,28 @@ function AdminPage() {
         // if a customer somehow gets to this page (shouldn't be possible), don't render
         <></> :
         // admin and superadmin view
-        <></>
+        <>
+          <div>
+            <Autocomplete
+              id="combo-box-demo"
+              options={userList}
+              getOptionLabel={(option) => option.email}
+              style={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="User List" variant="outlined" />}
+              getOptionSelected={(option, value) => option.id === value.id}
+              value={selectedUser}
+              onChange={(event, newValue) => setSelectedUser(newValue)}
+            />
+          </div>
+          <div>
+            <TextField
+                id="selectedUser"
+                label="Selected User"
+                value={selectedUser.name}
+                onChange={() => setSelectedUser({...selectedUser, name: event.target.value})}
+            />
+          </div>
+        </>
       }
     </section>
   );
