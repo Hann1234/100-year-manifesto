aae21e71baa0d85ac3fb0a7726e8580aec077806
Duy Dao <jimmydao@gmail.com>
2021-09-09 21:37:15 -0500

    Fixed when clicking on Logout in the burger menu to take user to login page.

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -25,7 +25,7 @@ function LogOutButton(props) {
       // is passed to it from it's parents through React props
       className={props.className}
       onClick={() => {
-        history.push("/homepage");
+        history.push("/login");
         handleLogOut()
         }
       }
